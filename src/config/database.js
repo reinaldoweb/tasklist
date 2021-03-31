194b2ab585608c34b1e87ccbf2be147e01780428
Reinaldo <reinaldoweb@hotmail.com>
2021-03-31 19:52:28 -0300

    Gerando hash

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '110278',
+  database: 'tasklist',
+  define:{
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true,
+  }
+};
